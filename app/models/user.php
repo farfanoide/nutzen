a1e1ff3bf53416c82a94414d2861079fb819f51e
Ivan Karl <ivan6258@gmail.com>
2016-01-28 15:33:19 -0300

    adds models folder and example User model

---
+++ app/models/user.php
@@ -0,0 +1,14 @@
+<?php namespace App\Models;
+
+use \Illuminate\Database\Eloquent\Model as Model;
+
+class User extends Model {
+
+  protected $table = 'users';
+
+  protected $fillable = [
+    'username',
+    'password'
+  ];
+
+}
